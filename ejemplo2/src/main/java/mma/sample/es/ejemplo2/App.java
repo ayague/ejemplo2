ce458929028095537a354aa85f3e4ddb2ce2ac79
agustin.yague <agustin.yague@upm.es>
2019-07-05 08:08:34 +0200

    Empty project created

---
+++ ejemplo2/src/main/java/mma/sample/es/ejemplo2/App.java
@@ -0,0 +1,13 @@
+package mma.sample.es.ejemplo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
