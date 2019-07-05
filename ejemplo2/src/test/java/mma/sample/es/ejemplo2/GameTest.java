f87b9f03ab87fae8ada9d01772db76448f4fbff4
agustin.yague <agustin.yague@upm.es>
2019-07-05 10:49:32 +0200

    Updated file structure

---
+++ ejemplo2/src/test/java/mma/sample/es/ejemplo2/GameTest.java
@@ -0,0 +1,49 @@
+package mma.sample.es.ejemplo2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public final void testGame() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testCreateRockQuestion() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testIsPlayable() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAdd() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testHowManyPlayers() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testRoll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testWasCorrectlyAnswered() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testWrongAnswer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
