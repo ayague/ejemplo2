ece8f90a1b40df2b6dc2f0006b09304e8ba40915
agustin.yague <agustin.yague@upm.es>
2019-07-05 10:04:09 +0200

    Adding Java Game files

---
+++ ejemplo2/src/test/java/mma/sample/es/ejemplo2/SomeTest.java
@@ -0,0 +1,13 @@
+package mma.sample.es.ejemplo2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SomeTest {
+
+	@Test
+	public void true_is_true() throws Exception {
+		assertTrue(true);
+	}
+}
